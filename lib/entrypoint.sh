de2bfe225ec26d66c926ea3408405accf42ba212
Dominik <dominik.alberski@gmail.com>
2020-02-18 11:41:42 +0100

    Update entrypoint.sh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-gem install rubocop-rspec
-gem install rubocop
+gem install rubocop \
+            rubocop-performance \
+            rubocop-rails \
+            rubocop-rspec
 
 ruby /action/lib/index.rb
